610b6e71ce4058a9b9741b571afbd2902d066c4d
ViniMagaa <118137226+ViniMagaa@users.noreply.github.com>
2023-06-27 23:27:59 -0300

    sla

--- CusroJS/exercicios/hora do dia/script.js
+++ CusroJS/exercicios/hora do dia/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('img')
     var data = new Date()
-    var hora = 18
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia

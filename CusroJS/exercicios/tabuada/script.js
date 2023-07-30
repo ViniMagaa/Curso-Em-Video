ce2980ae854394b3ee06f4ae43188342a14cf738
ViniMagaa <vinisantos2008vs@gmail.com>
2023-07-29 21:12:08 -0300

    Término do desafio da tabuada JS

--- CusroJS/exercicios/tabuada/script.js
+++ CusroJS/exercicios/tabuada/script.js
@@ -1,7 +1,11 @@
 function tabuada() {
-    let num = document.querySelector('#inum').value;
+    let num = document.getElementById('inum').value
+    let res = document.getElementById('res')
+    
+    res.innerText = ''
     
     for(let i = 1; i <= 10; i++) {
-        
+        let mult = i * num
+        res.innerHTML += `<p> ${i} x ${num} = ${mult} </p>`
     }
 }

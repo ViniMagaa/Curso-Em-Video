7316e76ed679af88d4b2fa18171d465763856c39
ViniMagaa <vinisantos2008vs@gmail.com>
2023-08-16 20:15:38 -0300

    Update contando.js

--- CusroJS/exercicios/contando/contando.js
+++ CusroJS/exercicios/contando/contando.js
@@ -1,18 +1,36 @@
 function contar() {
-    var inicio = parseInt(document.getElementById('numinicio').value)
-    var fim = parseInt(document.getElementById('numfim').value)
-    var passo = parseInt(document.getElementById('numpasso').value)
-    var text = document.getElementById('res')
+    let inicio = document.getElementById('numinicio')
+    let fim = document.getElementById('numfim')
+    let passo = document.getElementById('numpasso')
+    let res = document.getElementById('res')
 
-    text.innerHTML = ""
+    res.innerHTML = ""
 
-    if (passo <= 0) {
-        alert("O passo deve ser um número positivo!")
-    } else if (inicio >= fim) {
-        alert("O início deve ser menor que o fim!")
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível Contar!'
     } else {
-        for (var i = inicio; i <= fim; i += passo) {
-            text.innerHTML += i + " 👉 " 
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            alert('Passo inválido. Considerando: Passo = 1.')
+            p = 1
         }
+
+        if (i > f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            // Contagem decrescente
+            for (let c = i; c <= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += ` \u{1F3C1}`
     }
 }
